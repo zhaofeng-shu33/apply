76f58c3b34ea6815e1ae3df3ea9db21b99cbb6cb
zhaofeng-shu33 <616545598@qq.com>
2019-04-12 09:33:32 +0800

    add csrf support

--- django_volunteer_apply/models.py
+++ django_volunteer_apply/models.py
@@ -24,7 +24,7 @@ class Choice(models.Model):
         return self.activity.name + self.name
 
 class Person(models.Model):
-    name = models.CharField(max_length=9)
+    name = models.CharField(max_length=11)
     gender = models.CharField(max_length=1, choices=(('M','Male'),('F', 'Female')), default='M')
     choices = models.ManyToManyField(Choice, blank=True)
     def __str__(self):
